582bfa341119c4914b7370b42c7f06eacf8eebfc
Alan Reyes Luis <alannreyes02@gmail.com>
2025-04-08 12:33:33 -0600

    agregue la explicacion linea a linea

--- array/1. two sum.py
+++ array/1. two sum.py
@@ -9,12 +9,21 @@ You can return the answer in any order.
 
 class Solution:
     def twoSum(self, nums, target):
+        # inicializamos un diccionario para ir guardando valores
+        # y sus índices para no repetir elementos
         dic = {}  
+        # recorremos la lista de números
+        # con su indice y su elemento en el array
         for index1, num in enumerate(nums):
+            # calculamos el complemento de num actual
             complement = target - num
+            # verificamos si el complemento ya existe en el diccionario
             if complement in dic:
+                # si existe, retornamos el índice del complemento y el índice actual
                 return [dic[complement], index1]
-            dic[num] = index1 
+            # si no existe, lo agregamos al diccionario
+            dic[num] = index1      # dic {num: index1}
+        # si no encontramos la solución, retornamos None    
         return None
 
 
