4e816a79b73801af4f69c99eb5d27f5c3e876411
Alan Reyes Luis <alannreyes02@gmail.com>
2025-04-09 19:35:57 -0600

    explicacion de 189 Rotate array

--- array/189. Rotate Array.py
+++ array/189. Rotate Array.py
@@ -6,10 +6,16 @@ by k steps, where k is non-negative.
   """
   
 def rotate(nums, k):
+    # sacamos el modulo de k para evitar rotaciones innecesarias 
+    # si k es mayor que la longitud de nums 
+    # por ejemplo si nums tiene 7 elementos y k es 10,
+    # rotar 10 veces es lo mismo que rotar 3 veces
     k %= len(nums)
-    
+    # invertimos el array completo
     nums.reverse()
+    # invertimos los primeros k elementos
     nums[:k] = reversed(nums[:k])
+    # invertimos el resto de los elementos
     nums[k:] = reversed(nums[k:])
     return nums
 
