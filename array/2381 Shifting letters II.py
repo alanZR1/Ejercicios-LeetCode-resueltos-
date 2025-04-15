757bfcaaf000d443a3f539e31a3b2f97352a5757
Alan Reyes Luis <alannreyes02@gmail.com>
2025-04-14 19:55:17 -0600

    2381 Shifting letters II

--- array/2381 Shifting letters II.py
+++ array/2381 Shifting letters II.py
@@ -18,10 +18,12 @@ Return the final string after all such shifts to s are applied.
 class Solution(object):
     def shiftingLetters(self, s, shifts):
         n = len(s)
-        diff = [0] * (n + 1)  # Array de diferencias
+        diff = [0] * (n + 1)  # Array de diferencias -> diff = [ 0, 0, 0, 0] para n = 3
     
         # 1. Aplicar los cambios en el array de diferencias
+        #   Para cada desplazamiento... 
         for start, end, direction in shifts:
+            # ...si el desplazamiento es hacia adelante (1), incrementamos el inicio y decrementamos el final + 1
             if direction == 1:
                 diff[start] += 1
                 diff[end + 1] -= 1
@@ -31,10 +33,18 @@ class Solution(object):
 
         # 2. Obtener la suma acumulativa de desplazamientos
         shift = 0
+        # creamos una lista para almacenar los caracteres desplazados
         shifted_chars = []
         for i in range(n):
-            shift += diff[i]  # Aplicamos el desplazamiento acumulado
-            new_char = chr(((ord(s[i]) - ord('a') + shift) % 26) + ord('a'))  # Aplicamos el shift
+            # sumamos el desplazamiento acumulado
+            shift += diff[i]  
+            # new char es el nuevo caracter desplazado
+            # chr() convierte el valor ASCII en un caracter
+            # ord() convierte el caracter en su valor ASCII
+            # el nuevo caracter es el resultado de desplazar el caracter actual
+            # por el desplazamiento acumulado
+            # el %26 es para asegurarnos de que el desplazamiento no exceda el alfabeto
+            new_char = chr(((ord(s[i]) - ord('a') + shift) % 26) + ord('a')) 
             shifted_chars.append(new_char)
 
         return "".join(shifted_chars)
