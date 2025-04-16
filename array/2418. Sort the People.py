68b7bad87843eee0a16aeef52995faa5697f1892
Alan Reyes Luis <alannreyes02@gmail.com>
2025-04-16 14:59:26 -0600

    Sort the People

--- array/2418. Sort the People.py
+++ array/2418. Sort the People.py
@@ -10,13 +10,20 @@ Return names sorted in descending order by the people's heights.
   """
   
 def sortPeople(names, heights):
+    # convertir las listas en una lista de tuplas (nombre, altura)
     people = list(zip(names, heights))
+    # ordenar la lista de tuplas por altura en orden descendente
+    # el segundo elemento de la tupla es la altura
     people.sort(key = lambda x: x[1], reverse = True)
+    # crear una lista de nombres ordenados
     sorted_names = []
     for person in people:
+        # agregar el nombre a la lista de nombres ordenados
+        # el primer elemento de la tupla es el nombre
         sorted_names.append(person[0])
     return sorted_names
     
 names = ["Alice", "Bob", "Bob"]
 heights = [155, 185, 150]
 print(sortPeople(names, heights))
+# Output: ['Bob', 'Alice', 'Bob']
