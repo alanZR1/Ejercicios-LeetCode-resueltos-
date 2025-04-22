d7163b82386461b2c32927ca3ae04501ee59caf1
Alan Reyes Luis <alannreyes02@gmail.com>
2025-04-22 12:32:48 -0600

    update

--- linkedList/2. add Two Number.py
+++ linkedList/2. add Two Number.py
@@ -1,29 +1,52 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        l1new = "".join(map(str, l1[::-1]))  # Convierte cada número a string y revierte
-        l2new = "".join(map(str, l2[::-1]))
-        num1 = int(l1new)
-        num2 = int(l2new)
-        suma = num1 + num2
-        strSuma = str(suma)
-        reSuma = strSuma[::-1]
-        intSuma = int(reSuma)
-        lista = list(map(int, str(intSuma)))
-        return lista
+""" Medium
+
+
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order, and each of their nodes contains a single digit.
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero,
+except the number 0 itself.
+ """
+ 
+# en leetCode una linked-list se defina como una clase, 
+# por lo que se debe importar la clase ListNode
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val # valor del nodo
+        self.next = next # donde next es un puntero a otro nodo de la lista
+        
+        
+def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
+    node = ListNode()  # Nodo dummy para simplificar la construcción de la lista resultante
+    actual = node
+    acarreo = 0  # Acarreo inicial
     
-solution = Solution()
-
-l1 = [2,4,3]
-l2 = [5,6,4]
-
-print(solution.addTwoNumbers(l1, l2)) 
+    while l1 or l2 or acarreo:
+        # Obtener valores de los nodos actuales (o 0 si ya no hay nodos)
+        val1 = l1.val if l1 else 0
+        val2 = l2.val if l2 else 0
+        
+        # Calcular suma y acarreo
+        total = val1 + val2 + acarreo
+        # El nuevo acarreo es la parte entera de la división entre 10
+        acarreo = total // 10
+        # El dígito actual es el residuo de la división entre 10
+        # Esto asegura que el dígito esté entre 0 y 9
+        digit = total % 10
+        
+        # Crear nuevo nodo y avanzar
+        # Se asigna el valor del dígito al nodo actual
+        actual.next = ListNode(digit)
+        # Avanzar al siguiente nodo de la lista resultante
+        actual = actual.next
+        
+        # Mover l1 y l2 si existen
+        if l1:
+            # Avanzar al siguiente nodo de l1
+            l1 = l1.next
+        if l2:
+            # Avanzar al siguiente nodo de l2
+            l2 = l2.next
+    
+    return node.next  # El primer nodo real es node.next
