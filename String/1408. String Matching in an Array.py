bf9450ba0ecf1988d892e2283d9ae46f40e4cf95
Alan Reyes Luis <alannreyes02@gmail.com>
2025-04-28 19:25:13 -0600

    agregando comentarios para explicar el ejercicio

--- String/1408. String Matching in an Array.py
+++ String/1408. String Matching in an Array.py
@@ -12,12 +12,19 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[str]
         """
+        # creamos una lista vacia para almacenar los resultados
         ans = []
+        # ordenamos la lista de palabras por longitud
         words.sort(key=len)
+        # iteramos sobre la lista de palabras
         for i in range(len(words)):
+            # iteramos sobre las palabras que vienen despues de la palabra actual
             for j in range(i+1, len(words)):
+                # si la palabra actual es un substring de la siguiente
                 if words[i] in words[j]:
+                    # añadimos la palabra actual a la lista de resultados
                     ans.append(words[i])
+                    # salimos del bucle interno , ya no es necesario seguir buscando
                     break
         return ans
     
