e3052980cb16d84fdfbfb619153ff95b4128ba61
Alan Reyes Luis <alannreyes02@gmail.com>
2025-06-25 00:03:30 -0600

    agregando comentarios

--- Math/Reverse Integer.py
+++ Math/Reverse Integer.py
@@ -2,11 +2,11 @@ class Solution(object):
     
     def reverse(self, x):
         valor = abs(x)
-        
+        # convertimos el valor absoluto a string
         numstr = str(valor)
-        
+        # invertimos el string
         numstr = numstr[::-1]    
-        
+        # si el numero original es negativo, agregamos el signo negativo al inicio
         if x < 0:
             numstr = '-' + numstr
             
@@ -14,6 +14,7 @@ class Solution(object):
         
         return resultado if -2**31 <= resultado <= 2**31 - 1 else 0
     
+
     
 solucion = Solution()
 print(solucion.reverse(123)) 
